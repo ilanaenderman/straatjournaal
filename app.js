438da987e6771b0630915b8ca0978a1075cfc4fe
Ilana <ilanaenderman@hotmail.com>
2016-12-13 11:48:47 +0100

    made login work

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 // Setup Libraries
-const sequelize = require('sequelize')
-const express = require('express')
+const sequelize  = require('sequelize')
+const express 	 = require('express')
 const bodyParser = require('body-parser')
-const pug = require('pug')
-const pg = require('pg')
-const bcrypt = require('bcrypt-node')
-const sass = require('node-sass')
-const app = express()
+const pug 		 = require('pug')
+const pg 		 = require('pg')
+const session 	 = require('express-session')
+const bcrypt 	 = require('bcrypt-node')
+const sass 		 = require('node-sass')
+const app	 	 = express()
 
 
 // Requiring Modules
-const db = require(__dirname + '/modules/database.js')
+const db 		 = require(__dirname + '/modules/database.js')
 
 
 // Setting the pug views
@@ -19,22 +20,27 @@ app.set('views', __dirname + '/views')
 
 app.use(express.static('static'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+	secret: 'security is important',
+	resave: true,
+	saveUninitialized: false
+}))
 
 
 // Initialize Routes
-let homeRoute = require(__dirname + '/routes/home')
-let searchRoute = require(__dirname + '/routes/search')
+let homeRoute 	 = require(__dirname + '/routes/home')
+let searchRoute  = require(__dirname + '/routes/search')
 let contactRoute = require(__dirname + '/routes/contact')
-let aboutRoute = require(__dirname + '/routes/about')
-let adminRoute = require(__dirname + '/routes/administratie')
-let loginRoute = require(__dirname + '/routes/login')
+let aboutRoute 	 = require(__dirname + '/routes/about')
+let adminRoute 	 = require(__dirname + '/routes/administratie')
+let loginRoute 	 = require(__dirname + '/routes/login')
 
-app.use(homeRoute)
-app.use(searchRoute)
-app.use(contactRoute)
-app.use(aboutRoute)
-app.use(adminRoute)
-app.use(loginRoute)
+app.use( homeRoute)
+app.use( searchRoute)
+app.use( contactRoute)
+app.use( aboutRoute)
+app.use( adminRoute)
+app.use( loginRoute)
 
 
 // Listen port 8000
--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,48 @@
 // Login
 const express 	= require( 'express')
+const bcrypt 	= require('bcrypt-node')
 const router	= express.Router( )
 const db		= require(__dirname + '/../modules/database')
 
 // Get 
-router.get('/admin', (request, response) => {
-	response.render('login')
+router.get('/login', (request, response) => {
+	response.render('login', {message: request.query.message})
 })
 
 // Post
 router.post('/login', (request, response) => {
+	if(request.body.email.length === 0) {
+		response.redirect('/login?message=' + encodeURIComponent("U moet uw emailadres invullen."))
+		return
+	}
 
+	if(request.body.password.length === 0) {
+		response.redirect('/login?message=' + encodeURIComponent("U moet uw paswoord invullen."))
+		return
+	}
+
+	db.user.findOne({
+		where: {
+			email: request.body.email
+		}
+	}).then( admin => {
+		if( admin == 0) {
+			response.redirect('/login?message=' + encodeURIComponent("U moet een geldig emailadres invullen."))
+			return
+		}
+		else {
+			let hash = admin.password 
+			bcrypt.compare(request.body.password, hash, (err, res) => {
+				if (admin !== null && res == true) {
+					request.session.user = admin
+					response.redirect('/administratie')
+				} 
+				else {
+					response.redirect('/login?message=' + encodeURIComponent("Uw paswoord of emailadres is niet geldig."))
+				}
+			})
+		}
+	})
 })
 
 module.exports = router
